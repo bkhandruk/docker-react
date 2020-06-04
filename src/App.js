847aa1b4030a611f1a24ba95e1d3cf1dcd43a655
Bohdan Khandruk <bkhandruk@gmail.com>
2020-06-04 23:06:26 +0300

    fixing tests

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is changed!!!!!
+          learn react
         </a>
       </header>
     </div>
